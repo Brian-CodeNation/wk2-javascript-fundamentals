b23034cb0da3de7ef18e96c90109fe363262f2f2
Brian Harkins <brian.harkins@wearecodenation.com>
2024-08-05 13:32:35 +0100

    Added the code from the slides

--- dotnotation.js
+++ dotnotation.js
@@ -1,3 +1,7 @@
 console.log("hello world!".toUpperCase())
 
 console.log(Math.floor( Math.random()*6 ) )
+
+console.log("   --- - - --- |||||| ---")
+console.log("   --- - - --- |||||| ---")
+console.log("   --- - - --- |||||| ---")
