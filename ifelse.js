80f2d5d3dbd9fa48e163bfb85836be450f5b428a
Brian Harkins <brian.harkins@wearecodenation.com>
2024-08-06 11:05:45 +0100

    Added slide code

---
+++ ifelse.js
@@ -0,0 +1,31 @@
+// let weather = "SnoWy".toLowerCase();
+
+// if (weather == "sunny") {
+//     console.log("I'd better wear suncream")
+// } else if (weather == "rainy") {
+//     console.log("I'd bring a brolly")
+// } else if (weather == "snowy") {
+//     console.log("I will get my sledge out")
+// } else {
+//     console.log("It could go either way")
+// };
+
+// let diceNum = Math.floor(Math.random()*6);
+// console.log(`You rolled a ${diceNum}`)
+
+// if (diceNum >= 4) {
+//     console.log("YOU WIN!!!!!")
+// } else {
+//     console.log("Try again")
+// }
+
+let place = "Manchester";
+let weather = "Sunny";
+
+if (place == "Manchester" && weather == "Sunny" ) {
+    console.log("Check again....")
+} else if (place == "Manchester" && weather == "Rainy") {
+    console.log("Obviously....")
+} else {
+    console.log("What?? It isn't raining?")
+}
