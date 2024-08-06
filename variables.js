718ca5ca9a541444e04500df567c311bb677f1c9
Brian Harkins <brian.harkins@wearecodenation.com>
2024-08-06 09:58:45 +0100

    added activity comments

---
+++ variables.js
@@ -0,0 +1,30 @@
+// let count = 10;
+
+// count *= 2;
+
+// console.log(count)
+
+// let favouriteDrink = "coffee";
+
+// console.log("My favourite drink is " + favouriteDrink);
+
+let name = 'Spongebob';
+let age = 27;
+let favDrink = 'Hot Chocolate';
+
+// console.log("Hi. My name is " + name + ". My age is " + age + " and my favourite drink is " + favDrink)
+
+// This is using the TEMPLATE LITERALS
+console.log(`Hi. My name is ${name}. My age is ${age} and my favourite drink is ${favDrink}`)
+
+age = 102;
+favDrink = "Gin and tonic";
+
+console.log(`Hi. My name is ${name}. My age is ${age} and my favourite drink is ${favDrink}`)
+
+
+// ACTIVITY 1
+
+
+
+// ACTIVITY 2
